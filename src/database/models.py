bab2f99f6d4611fa5c5e255a1e77eab80febbc24
Andrii Popov <97399846+Andrii0207@users.noreply.github.com>
2025-03-30 15:34:36 +0300

    add requirements prev task

---
+++ src/database/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, Boolean, func, Table
+from sqlalchemy.orm import relationship, mapped_column, Mapped, DeclarativeBase
+from sqlalchemy.sql.schema import ForeignKey, PrimaryKeyConstraint
+from sqlalchemy.sql.sqltypes import DateTime, Date
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Contact(Base):
+    __tablename__ = "contacts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    phone = Column(String)
+    birth_date = Column(Date)
+    additional_data = Column(String, nullable=True)
